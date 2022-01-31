6a91a1768970500aeb1a5e0d3c30879a5f8aa5df
Ziamrurth <93211239@mail.ru>
2022-01-31 20:42:42 +0300

    Template project created

---
+++ GeoProject/GeoProject/Helpers/JsonReader.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoProject.Helpers {
+    class JsonReader {
+        public void LoadJson(string fileName)
+        {
+            using (StreamReader r = new StreamReader(fileName))
+            {
+                string json = r.ReadToEnd();
+                List<Polygon> polygons = JsonConvert.DeserializeObject<List<Polygon>>(json);
+            }
+        }
+    }
+
+    public class Polygon {
+        public string type;
+        public Features features;
+    }
+
+    public class Features {
+        public string type;
+        public Props properties;
+        public Geometry geometry;
+    }
+
+    public class Props {
+        public string name;
+        public float buffer;
+    }
+
+    public class Geometry {
+        public string type;
+        public Coordinates coordinates;
+    }
+
+    public class Coordinates {
+        public float x;
+        public float y;
+    }
+}
